d0889647306bb93f7ed5380cb2b0c70d801aa34a
Lovisa Bohman <lovisabo@hotmail.com>
2021-01-27 10:34:05 +0100

    Add more specific get methods for register-link

--- cypress/integration/SignUp.js
+++ cypress/integration/SignUp.js
@@ -2,10 +2,9 @@ describe("Registration form", () => {
 
     it("Registration successful", () => {
         cy.visit("/");
-        cy.get("a").click();
-        
+        cy.get('a[href*="register"]').click();
+
         cy.contains("Registrera!");
-        cy.get("form");
         cy.get('input[name="username"]').type("UncoolUser").should("have.value", "UncoolUser");
         cy.get('input[name="email"]').type("mail@mail.com").should("have.value", "mail@mail.com");
         cy.get('input[name="password"]').type("asd").should("have.value", "asd");
@@ -16,71 +15,75 @@ describe("Registration form", () => {
 
     it("Registration fail - invalid username with other fields empty", () => {
         cy.visit("/");
-        cy.get("a").click();
-        
+        cy.get('a[href*="register"]').click();
+
         cy.contains("Registrera!");
         cy.get('input[name="username"]').type(" ").should("have.value", " ");
+
         cy.get("form").submit().end();
     })
 
     it("Registration fail - invalid username with other fields filled", () => {
         cy.visit("/");
-        cy.get("a").click();
+        cy.get('a[href*="register"]').click();
         
         cy.contains("Registrera!");
         cy.get('input[name="username"]').type(" ").should("have.value", " ");
         cy.get('input[name="email"]').type("mail@mail.com").should("have.value", "mail@mail.com");
         cy.get('input[name="password"]').type("asd").should("have.value", "asd");
         cy.get('input[name="passwordConfirm"]').type("asd").should("have.value", "asd");
+
         cy.get("form").submit().end();
     })
 
     it("Registration fail - invalid email with other fields empty", () => {
         cy.visit("/");
-        cy.get("a").click();
+        cy.get('a[href*="register"]').click();
 
         cy.contains("Registrera!");
         cy.get('input[name="username"]').type("UncoolUser").should("have.value", "UncoolUser");
         cy.get('input[name="email"]').type("mail@").should("have.value", "mail@");
+
         cy.get("form").submit().end();
     })
 
     it("Registration fail - invalid email with other fields filled", () => {
         cy.visit("/");
-        cy.get("a").click();
+        cy.get('a[href*="register"]').click();
 
         cy.contains("Registrera!");
         cy.get('input[name="username"]').type("UncoolUser").should("have.value", "UncoolUser");
         cy.get('input[name="email"]').type("mail@").should("have.value", "mail@");
         cy.get('input[name="password"]').type("asd").should("have.value", "asd");
         cy.get('input[name="passwordConfirm"]').type("asd").should("have.value", "asd");
+
         cy.get("form").submit().end();
     })
 
     it("Registration fail - non-matching password", () => {
         cy.visit("/");
-        cy.get("a").click();
+        cy.get('a[href*="register"]').click();
 
         cy.contains("Registrera!");
         cy.get('input[name="username"]').type("UncoolUser").should("have.value", "UncoolUser");
         cy.get('input[name="email"]').type("mail@mail.com").should("have.value", "mail@mail.com");
         cy.get('input[name="password"]').type("asd").should("have.value", "asd");
         cy.get('input[name="passwordConfirm"]').type("asdf").should("have.value", "asdf");
-        cy.get("form").submit().end();
 
+        cy.get("form").submit().end();
     })
 
     it("Registration fail - non-matching password II", () => {
         cy.visit("/");
-        cy.get("a").click();
+        cy.get('a[href*="register"]').click();
 
         cy.contains("Registrera!");
         cy.get('input[name="username"]').type("UncoolUser").should("have.value", "UncoolUser");
         cy.get('input[name="email"]').type("mail@mail.com").should("have.value", "mail@mail.com");
         cy.get('input[name="password"]').type("asdf").should("have.value", "asdf");
         cy.get('input[name="passwordConfirm"]').type("asd").should("have.value", "asd");
-        cy.get("form").submit().end();
 
+        cy.get("form").submit().end();
     })
 
 })
