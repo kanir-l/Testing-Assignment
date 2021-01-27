90839a78f204ebf155689b1dbf601743db1d8577
FredrikAndre <fredrik.he.andre@gmail.com>
2021-01-27 10:37:32 +0100

    Added Cypress test to presentation.js

---
+++ cypress/integration/Guestbook.js
@@ -0,0 +1,28 @@
+describe("Guestbook", () => {
+
+    it("Guestbook Fill in Message", () => {
+
+        // Error too short message
+        cy.visit("/guestbook.html") 
+        cy.get("form")
+        cy.get("textarea").type("he")
+        cy.get("form").submit()
+        cy.contains("kort")
+
+        // Successful message
+        cy.get("form")
+        cy.get("textarea").clear().type("hej")
+        cy.get("form").submit()
+        cy.contains("Ta bort")
+
+        // Remove succesful message
+        cy.get("a").contains("Ta bort").click()
+        cy.get("form")
+
+        // Back to Start page
+        cy.get('a[href*="start"]').click()
+        cy.url().should("include", "start").end()
+
+    })
+
+})
